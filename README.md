d04b3581b2f778e3bc883d4e15f95d4a30408b79
Brendo Vale <brendo.vale02@gmail.com>
2024-07-01 03:02:32 -0400

    feat: implement tokens package

---
+++ README.md
@@ -0,0 +1,17 @@
+# Componentes
+
+- [ ] Text
+- [ ] Heading
+- [ ] Box
+- [ ] Button
+- [ ] TextInput
+- [ ] TextArea
+- [ ] Checkbox
+- [ ] Avatar
+- [ ] MultiStep
+
+# Tokens
+
+Dependencies
+dev
+TSUP - Builda o codigo em diversos formatos como por exemplo commonjs, ecmascript, definição de tipos, e performatico
---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray50: '#F6F6F6',
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  tui300: '#3F66EE',
+  tui500: '#3253C7',
+  tui700: '#1A2B66',
+  tui900: '#061342',
+}
